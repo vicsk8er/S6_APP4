b9137dd2490d79ecfc2fba309f9c90d4df4b6756
vicsk8er <victor.larose@hotmail.com>
2026-07-04 16:29:23 -0400

    remove include

--- src/rx_tasks.cpp
+++ src/rx_tasks.cpp
@@ -4,7 +4,6 @@
 #include "receive_frame.h"
 #include "send_frame.h"
 #include "utils/frame_buffer.h"
-#include "manchester/manchester_driver.h"
 #include <stdio.h>
 
 static ReceptionContext rxContext;
